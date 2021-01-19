5bf4fd96542cd4aba6debc55caa86d8a9c0273c6
pclightyear <ee91608@gmail.com>
2021-01-19 21:52:56 +0800

    Finish API 1, 2, 8
    Finish API
    1. getProfile
    2. updateProfile
    8. getJoinProject

--- STARS/star/urls.py
+++ STARS/star/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('test_db/', views.test_db),
-    path('profile/', views.profile),
+    path('test_db', views.test_db),
+    path('profile', views.profile),
     path('profile/submit', views.profile_submit),
     path('register/submit', views.register_submit),
     path('login/submit', views.login_submit),
-    path('home/', views.home)
+    path('home', views.home),
+    path('join-project', views.join_project),
+    # path('join-project/project-info-target', view.join_project_info)
 ]
--- STARS/star/views.py
+++ STARS/star/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.db import connection
+from django.utils.decorators import method_decorator
 # Create your views here.
-
 
 """
     Utils
@@ -16,6 +16,7 @@ def test_db(request):
     print(data)
 
     return HttpResponse("test_db")
+
 def processData(cursor):
     "Return all rows from a cursor as a dict"
     columns = [col[0] for col in cursor.description]
@@ -34,10 +35,61 @@ def index(request):
 """
 
 def profile(request):
-    return HttpResponse("get profile")
+    uid = request.GET['uid']
+    sql = \
+    """
+        SELECT * 
+        FROM user_db 
+        WHERE uid = {uid} 
+    """.format(uid=uid)
+
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        res = processData(cursor)
+
+    print(res)
+
+    return HttpResponse("get profile: {}".format(res))
 
 def profile_submit(request):
-    return HttpResponse("update profile sucess")
+    # @method_decorator(csrf_exempt, name='dispatch')
+    uid = request.POST['uid']
+    username = request.POST['username']
+    name = request.POST['name']
+    email = request.POST['email']
+    affiliation = request.POST['affiliation']
+    title = request.POST['title']
+    country = request.POST['country']
+
+    sql = \
+    """
+        UPDATE user_db
+        SET username = '{username}', name = '{name}', email = '{email}', affiliation = '{affiliation}', title = '{title}', country = '{country}'
+        WHERE uid = {uid}
+    """.format(
+        username=username, 
+        name=name, 
+        email=email, 
+        affiliation=affiliation, 
+        title=title, 
+        country=country, 
+        uid=uid
+    )
+
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        sucess = cursor.rowcount is 1
+        res = {
+            "sucess": sucess
+        }
+    
+    print(res)
+
+    if sucess:
+        return HttpResponse("update profile sucess: {}".format(res))
+    else:
+        return HttpResponse("update profile fail: {}".format(res))
+    
 
 """
     Register
@@ -64,6 +116,44 @@ def home(request):
 """
     Project
 """
+def join_project(request):
+    uid = request.GET['uid']
+    sql = \
+    """
+        SELECT p.pid, p.title, p.project_type, p.description
+        FROM project_db as p
+        INNER JOIN (
+            SELECT pid
+            FROM participate_db 
+            WHERE uid = {uid} 
+        ) as j
+        ON p.pid = j.pid
+    """.format(uid=uid)
+
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        res = processData(cursor)
+
+    print(res)
+
+    return HttpResponse("get join projects: {}".format(res))
+
+def join_project_info(request):
+    # pid = request.GET['pid']
+    # sql = \
+    # """
+    #     SELECT *
+    #     FROM project_db
+    #     WHERE pid = {pid}
+    # """.format(pid=pid)
+
+    # with connection.cursor() as cursor:
+    #     cursor.execute(sql)
+    #     res = processData(cursor)
+
+    # print(res)
+
+    # return HttpResponse("get join project info: {}".format(res))
 
 """
     Schedule
